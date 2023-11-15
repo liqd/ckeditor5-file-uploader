f284c2bd34928dcc48eb60ae0175e0ced37a2fb0
Julian Dehm <j.dehm@liqd.net>
2023-11-15 11:26:50 +0100

    remove fileuploadprogress for now

--- src/fileuploader.ts
+++ src/fileuploader.ts
@@ -1,11 +1,10 @@
 import { Plugin } from 'ckeditor5/src/core';
 import FileUploadEditing from './fileuploadediting';
 import FileUploadUI from './fileuploadui';
-import FileUploadProgress from './fileuploadprogress';
 
 export default class FileUploader extends Plugin {
 	public static get requires() {
-		return [ FileUploadEditing, FileUploadUI, FileUploadProgress ] as const;
+		return [ FileUploadEditing, FileUploadUI ] as const;
 	}
 
 	public static get pluginName() {
